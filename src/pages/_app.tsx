5a35fd633ca27fd708d1068b7654446c7851bdbd
Reo Ito <leo.0210.ito@gmail.com>
2023-05-11 00:06:01 +0900

    Google Analyticsを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,48 @@
 import "@/styles/globals.css"
 import { ChakraProvider } from "@chakra-ui/react"
 import type { AppProps } from "next/app"
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (!window) return
+      // @ts-ignore
+      window.gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
+        page_path: url
+      })
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    router.events.on("hashChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+      router.events.off("hashChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script
+        async
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
